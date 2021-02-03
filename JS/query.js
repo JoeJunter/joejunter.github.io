88dd3cd7b3e2e7cce958c968a47545c25e798502
JoeJunter <73821656+JoeJunter@users.noreply.github.com>
2021-02-03 12:26:56 +0000

    changed some bits

--- JS/query.js
+++ JS/query.js
@@ -1,52 +1,6 @@
 import sqlite3 from 'sqlite3';
-//import Console from 'console';
-//import returnQueryResults from './utils';
-
-//uncomment these import statements once you are up online and can use modules
-
-
-
-const extractKeys = array => {
-  if (array.length === 0) {
-    return [];
-  }
-  let keys = Object.keys(array[0]);
-  let isCopy = array.every(element => {
-    if (typeof element !== 'object') {
-      throw new Error(`Array must be made entirely of objects`);
-    }
-    return Object.keys(element).every(key => keys.indexOf(key) !== -1);
-  })
-  if (isCopy) {
-    return keys;
-  } else {
-    throw new Error(`Array's object's keys do not match`);
-  }
-}
-
-const returnQueryResults = array => {
-  if (typeof array !== 'object') {
-    console.log(array);
-    return array;
-  }
-  if (!Array.isArray(array)) {
-    array = [array]
-  }
-  let keys = extractKeys(array);
-  let output = [];
-  output.push(`\t${keys.join('\t')}`);
-  array.forEach(row => {
-    output.push(`\t${Object.keys(row).map(key => row[key]).join('\t')}`);
-  });
-  output = output.join('\n');
-  console.log(output);
-  return output;
-}
-
-
-// the above is all in the 'utils' file, which will be its on module once we're online
-
-
+import Console from 'console';
+import returnQueryResults from './utils';
 
 const db = new sqlite3.Database('./RPIData.db');
 
